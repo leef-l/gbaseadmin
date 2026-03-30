4e33e517cbaa7870383334615fdbe37172f78129
燧火科技有限公司 <suihuokeji@qq.com>
2026-03-31 03:28:39 +0800

    input高度

---
+++ vue-vben-admin/apps/web-antd/src/views/play/statistics/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Page } from '@vben/common-ui';
+</script>
+
+<template>
+  <Page auto-content-height>
+    <div class="flex h-full items-center justify-center text-lg text-gray-400">
+      数据统计（开发中）
+    </div>
+  </Page>
+</template>
