50a7a4bf559f997cfb213a6d9acf5cdf8e14d011
燧火科技有限公司 <suihuokeji@qq.com>
2026-04-01 10:59:25 +0800

    去掉这个，不知道有什么用

--- wap-ui/config/dev.ts
+++ wap-ui/config/dev.ts
@@ -4,9 +4,6 @@ export default {
   env: {
     NODE_ENV: '"development"',
   },
-  defineConstants: {
-    'process.env.TARO_APP_API': '""',
-  },
   mini: {},
   h5: {},
 } satisfies UserConfigExport;
